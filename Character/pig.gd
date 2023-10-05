53959a61ce92c8a7b28d40b3f9936431bf556e26
Pikalyx <pikalegend789@gmail.com>
2023-10-05 13:31:09 -0400

    Pig A.I. Progress
    - movement
    - is able to turn around at ledges
    - no animations

---
+++ Character/pig.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const movement_speed : float = 30.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var facing_left=true
+
+func _physics_process(delta):
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	detect_turn_around()
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	if facing_left:
+		velocity.x = -movement_speed
+		
+		
+	elif not facing_left:
+		velocity.x = movement_speed
+		
+
+	move_and_slide()
+	
+func detect_turn_around():
+	if not $CollisionShape2D/RayCast2D.is_colliding() and is_on_floor():
+		facing_left= !facing_left
+		scale.x = -scale.x
