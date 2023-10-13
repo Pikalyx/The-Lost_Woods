3f6a8f5403285ba87c8a1aa29fd95cc9c36d6bec
mercylagosm <gmaderamunoz@muhlenberg.edu>
2023-10-13 17:13:31 -0400

    Level Transition
    Version where character can move from one level to another

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_new_game_button_button_up():
-	get_tree().change_scene_to_file("res://test_level.tscn")
+	get_tree().change_scene_to_file("res://test_level1.tscn")
 
 
 func _on_credits_button_button_up():
