ce2cf0908779322d1c0c5b123e9613b1722e1723
Pikalyx <78184925+Pikalyx@users.noreply.github.com>
2023-10-26 19:42:36 -0400

    Pig changes
    - changed layers and mask

---
+++ Character/walk.gd
@@ -0,0 +1,24 @@
+extends Node
+
+
+class_name State
+
+@export var can_move : bool = true
+
+var character : CharacterBody2D
+var next_state : State
+var playback : AnimationNodeStateMachinePlayback
+
+signal interrupt_state(new_state : State)
+
+func state_process(delta):
+	pass
+
+func state_input(event : InputEvent):
+	pass
+
+func on_enter():
+	pass
+	
+func on_exit():
+	pass
