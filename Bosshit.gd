1bb94157c1f01af07e1c131d9cd1ef8ab8217cc0
Giovanni Sanchez <108995267+giovanni747@users.noreply.github.com>
2023-11-30 19:08:38 -0500

    Revert "s"
    This reverts commit 9fbbad16705f9c78a0fe7b1df5b12e837c6f378c.

--- Bosshit.gd
+++ Bosshit.gd
@@ -1,34 +1,11 @@
 extends State
 
 
-class_name BossHit
-
-@export var damageable : Damageable
-@export var dead_state : State
-@export var dead_animation_node : String = "dead"
-@export var knockback_speed : float = 100.0
-@export var return_state : State
-
-@onready var timer : Timer = $Timer
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	damageable.connect("on_hit", on_damageable_hit)
-	
-func on_enter():
-	timer.start()
-	
-func on_damageable_hit(node : Node , damage_amount : int, knockback_direction : Vector2):
-	if(damageable.health > 0):
-		character.velocity = knockback_speed * knockback_direction
-		emit_signal("interrupt_state", self)
-	else:
-		emit_signal("interrupt_state",dead_state)
-		playback.travel(dead_animation_node)
-		
-func on_exit():
-	character.velocity = Vector2.ZERO
-	
+	pass # Replace with function body.
 
 
-
-func _on_timer_timeout():
-	next_state = return_state
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
