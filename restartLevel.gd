fa501f3fde3df986a486a1a5be0134edff76b7ae
Pikalyx <78184925+Pikalyx@users.noreply.github.com>
2023-12-03 17:02:29 -0500

    change scripts again
    stopped next level scene from activating when player leaves level bounds, restart level doesn't work

--- restartLevel.gd
+++ restartLevel.gd
@@ -1,12 +1,5 @@
 extends Area2D
 
 func _on_body_entered(body):
-	#For testing
-	#print("Level Cleared")
-	var current_scene_file = get_tree().current_scene.scene_file_path
-	#For testing
-	
-	
-	
-	
+	var current_scene_file = get_tree().current_scene.scene_file_path	
 	SceneTransition.change_scene_to_file(current_scene_file)
