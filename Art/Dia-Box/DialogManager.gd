0a4eb36de512bc41a82bf5d420121307ff26a866
Scrathy <144823798+Scrathy@users.noreply.github.com>
2023-12-02 23:33:25 -0500

    Update DialogManager.gd

--- Art/Dia-Box/DialogManager.gd
+++ Art/Dia-Box/DialogManager.gd
@@ -1,11 +1,30 @@
 extends Node
 
+@onready var text_box_scene = preload("res://Art/Dia-Box/text-box.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var dialog_lines: Array[String] = []
+var current_line_index = 0
 
+var text_box
+var text_box_postion: Vector2
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+var is_dialog_active = false
+var van_advance_line = false
+
+func start_dialog(position: Vector2, lines: Array[String]):
+	if is_dialog_active:
+		return
+		
+	dialog_lines = lines
+	text_box_position = position
+	_show_text_box()
+	
+	is_dialog_active = true
+	
+func _show_text_box():
+	text_box = text_box_scene.instantiate()
+	text_box.finished_displaying.connect(_on_text_box_finished_displaying)
+	get_tree().root.add_child(text_box)
+	text_box.global_position = text_box_position
+	text_box.display_text(dialog_lines[current_line_index])
+	can_advance_line = flase
