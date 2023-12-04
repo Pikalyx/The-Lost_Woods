a77fb3ada0f9cc6fc7876c6a0b8c9f5a80e89037
100000Doorknobs <100000doorkonbs@gmail.com>
2023-12-04 08:56:49 -0500

    New Trailer

---
+++ Hive.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var spawner : PackedScene
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+func _ready():
+	spawner = ResourceLoader.load("res://Trailer.tscn")
+
+func _physics_process(delta):
+	if $SpawnTimer.is_stopped() == true:
+		$SpawnTimer.start()
+
+
+func _on_spawn_timer_timeout():
+	var spawn = spawner.instantiate()
+	spawn.set_global_position(self.get_global_position())
+	get_parent().add_child(spawn)
+	
--- Trailer.gd
+++ Trailer.gd
@@ -1,32 +1,85 @@
-extends Sprite2D
+extends CharacterBody2D
 
 var stalkOffset : float
-var windUp = true
+@export var state : String = "Launch"
+@export var damage = 1
 var directionCommit : float
+var homeSpeed = 400
+var cooldown = false
+var direction : Vector2
+var angle : float
+var entering : bool
 @onready var player = get_parent().get_node("Player")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AudioStreamPlayer2D.play()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if windUp == true:
+	if state == "Launch":
+		if not is_on_floor() and not is_on_wall():
+			if cooldown == false:
+				look_at(player.get_position())
+				angle = fmod(self.get_rotation(), 2 * PI)
+				direction = Vector2(cos(angle), sin(angle))
+				if angle >= PI/2 or angle <= -PI/2:
+					$Sprite2D.flip_v = true
+					$Sprite2D.offset.y = 17
+				else:
+					$Sprite2D.flip_v = false
+					$Sprite2D.offset.y = 0
+			#animation.play("home")
+				cooldown = true
+				print("Trailer cooldown is ", cooldown, (player.get_position() - self.get_position()))
+			self.set_position(self.get_position() + (direction * homeSpeed * delta))
+		else:
+			queue_free()
+			print("Trailer die!")
+		move_and_slide()
+
+	elif state == "Latch":
+		if entering == true:
+			rotation = PI
+			$AudioStreamPlayer2D.play()
+			self.set_position(Vector2(stalkOffset, player.get_position().y))
+			entering = false
 		#var player = get_parent().get_node("Player")
-		if player.direction.x == 1:
-			stalkOffset = (player.get_position().x - 50)
-		elif player.direction.x == -1:
-			stalkOffset = (player.get_position().x + 50)
-		self.set_position(Vector2(stalkOffset, player.get_position().y))
-	elif windUp == false:
+		elif entering == false:
+			if not is_on_floor() and not is_on_ceiling():
+				if player.sprite.flip_h == false:
+					stalkOffset = (player.get_position().x - 25)
+				elif player.sprite.flip_h == true:
+					stalkOffset = (player.get_position().x + 25)
+				self.set_position(Vector2(stalkOffset, player.get_position().y))
+			move_and_slide()
+	
+	elif state == "Hatch":
 			self.set_position(Vector2(self.get_position().x +(500 * directionCommit * delta) , self.get_position().y))
 
 
 func _on_audio_stream_player_2d_finished():
-	windUp = false
-	var player = get_parent().get_node("Player")
+	state = "Hatch"
 	if player.sprite.flip_h == false:
 		directionCommit = 1
 	elif player.sprite.flip_h == true:
 		directionCommit = -1
+
+
+func _on_area_2d_body_entered(body):
+	if state == "Launch":
+		state = "Latch"
+		entering = true
+	elif state == "Hatch":
+		for child in body.get_children():
+			if child is Damageable:
+				print(self, " is hitting ", child)
+				var direction_to_damageable = (body.global_position - get_parent().global_position) 
+				var direction_sign = sign(direction_to_damageable.x)
+
+				if(direction_sign > 0):
+					child.hit(damage, Vector2.RIGHT)
+				elif(direction_sign < 0):
+					child.hit(damage, Vector2.LEFT)
+				else:
+					child.hit(damage, Vector2.ZERO)
