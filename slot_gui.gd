0d0fa0bc9dc0ca58c3e0c89abd7b35adfc0ed7ff
Giovanni Sanchez <108995267+giovanni747@users.noreply.github.com>
2023-12-03 23:59:11 -0500

    w

--- slot_gui.gd
+++ slot_gui.gd
@@ -2,21 +2,31 @@ extends Panel
 
 @onready var backgroundSprite: Sprite2D = $background
 @onready var itemSprite: Sprite2D = $CenterContainer/Panel/item
-
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var button: Button = $CenterContainer/Panel/use
 
 
 func update(item: inventoryItem):
 	if !item:
 		backgroundSprite.frame = 0
 		itemSprite.visible = false
+		button.visible = false
 	else:
 		backgroundSprite.frame = 1
 		itemSprite.visible = true
+		button.visible = true
 		itemSprite.texture = item.texture
 		
+		
+		
+func health_me(item: inventoryItem):
+	if item.name ==  "HealthFlute":
+		return true
 
-
-func _on_use_pressed(item: inventoryItem):
-	if !item:
-		print("nothing sorry")
+			
+		
+			
+			
+		
+		
 	
