f85d23db96e22b0ec7412926ce97c9c28051c85c
Scrathy <lukedukette@gmail.com>
2023-12-04 10:15:27 -0500

    Update Sign.gd

--- Sign.gd
+++ Sign.gd
@@ -8,3 +8,10 @@ const lines: Array[String] = [
 	"Hello"
 ]
 
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+	
+
+func _on_interact():
+	DialogManager.start_dialog(global_position, lines)
+	await DialogManager.dialog_finished
