e017cfbb4db216bb4a40ac4056aff4f267a20289
lexi-con <alexaschriver@gmail.com>
2023-12-12 15:49:28 -0500

    Update Next Level.gd
    Added a line that routes the player to the boss once the tutorial is completed.

--- Next Level.gd
+++ Next Level.gd
@@ -6,9 +6,12 @@ func _on_body_entered(body):
 	#print("Level Cleared")
 	var current_scene_file = get_tree().current_scene.scene_file_path
 	#For testing
-	
+
 	var next_level_number = current_scene_file.to_int() + 1
-	var next_level_path = "res://test_level" + str(next_level_number) + ".tscn"
+	#var next_level_path = "res://test_level" + str(next_level_number) + ".tscn"
+	
+	#For demonstration
+	var next_level_path = "res://carter_level_1.tscn"
 	
 	SceneTransition.change_scene_to_file(next_level_path)
 
